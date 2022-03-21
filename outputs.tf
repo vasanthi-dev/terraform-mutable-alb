ca50b88ed6623ba44d1d5837e2a95a8ca07dc2bd
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-21 11:44:13 -0400

    frontend

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,10 @@ output "PRIVATE_LISTENER_ARN" {
   value = aws_lb_listener.private.arn
 }
 
+output "PUBLIC_LB_DNSNAME" {
+  value = aws_lb.public.dns_name
+}
+
+output "PRIVATE_LB_DNSNAME" {
+  value = aws_lb.private.dns_name
+}
